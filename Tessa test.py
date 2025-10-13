a1e5fb171473b4abcefde73d8def76d11f15293e
chiomandu <chioma.ndu@ada.ac.uk>
2025-10-13 14:00:59 +0000

    Learning from tessa

---
+++ Tessa test.py
@@ -0,0 +1,15 @@
+print ("hello")
+age=int(input ("what is your age "))
+#print(str(age)+" this is your age")
+
+if age<=30:
+    print("you are young")
+elif age<=60:
+    print("You are middle age")
+else:
+    print("You are old")
+
+# int(age) is 151
+# str(age) is "15"
+# float(age) is 15.0
+
