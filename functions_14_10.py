35100cf508cecab71d621f80d08a1396064f1a71
chiomandu <chioma.ndu@ada.ac.uk>
2025-10-16 13:12:30 +0000

    examples

---
+++ functions_14_10.py
@@ -0,0 +1,12 @@
+def add (a, b):
+    return a + b
+result=add (10 , 4)
+print (result)
+
+user_input=int(input("Enter celsius? "))
+def conversion_to_fah (celsius):
+    return celsius*1.8+32
+fah=conversion_to_fah (user_input)
+print (fah)
+
+
